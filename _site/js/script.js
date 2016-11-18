02513bb48e32d31e9ef4eabc9d158b3be74f856c
Bracketeer <brent.stradling@gmail.com>
2016-11-18 02:33:57 -0600

    initial commit

---
+++ _site/js/script.js
@@ -0,0 +1,5 @@
+$('.view-more').click(function(){
+	$('#more-content').toggleClass('expand-content');
+	$('#more-content').toggleClass('hide-content');
+});
+new Vivus('brent', {duration: 2000, file: '/img/brent_animated.svg'});
